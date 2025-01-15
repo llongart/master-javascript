27a46c6a6f5974da540d15624c646fd28bac69e4
llongart <llongart413@gmail.com>
2025-01-14 22:34:39 -0400

    Ejercicios bloque 4

--- 03-javascript/17-ejercicios-4/js/ejercicio31.js
+++ 03-javascript/17-ejercicios-4/js/ejercicio31.js
@@ -1,46 +1,36 @@
 /*
 Ejercicio 31:
 
-Crea un semáforo con tres luces (rojo, amarillo y verde)
-Haz los circulos del semaforo con HTML y CSS
+    Crea un semáforo con tres luces (rojo, amarillo y verde)
+    Haz los circulos del semaforo con HTML y CSS
 
-Cuando un peaton quiere cruzar la calle debe pulsar un botón.
-Crea un botón para cambiar el color de las luces del semáforo
-secuencialmente (de rojo a verde, pasando por amarillo).
+    Cuando un peaton quiere cruzar la calle debe pulsar un botón.
+    Crea un botón para cambiar el color de las luces del semáforo
+    secuencialmente (de rojo a verde, pasando por amarillo).
 
-Entonces, 
-El semáforo siempre empieza en rojo.
-Cuando haces click en el botón pasará a amarillo.
-Al hacer click otra vez, pasa a verde, y así todo el rato.
+    Entonces, 
+    El semáforo siempre empieza en rojo.
+    Cuando haces click en el botón pasará a amarillo.
+    Al hacer click otra vez, pasa a verde, y así todo el rato.
 */
 
-function cambiarColor() {
-    // let semaforo = document.querySelector('#semaforo');
-    // let rojo = document.querySelector('#rojo');
-    // let amarillo = document.querySelector('#amarillo');
-    // let verde = document.querySelector('#verde');
+let btnCambiar = document.querySelector('#btnCambiar');
+let luces = [...document.querySelectorAll('.luz')];
 
-    switch (semaforo.className){
-        case 'semaforo luz-roja':
-            semaforo.className = 'semaforo luz-amarilla';
-            rojo.classList.add('apagado');
-            amarillo.classList.remove('apagado');
-            break;
-        case 'semaforo luz-amarilla':
-            semaforo.className = 'semaforo luz-verde';
-            amarillo.classList.add('apagado');
-            verde.classList.remove('apagado');
-            break;
-        case 'semaforo luz-verde':
-            semaforo.className = 'semaforo luz-verde-amarilla';
-            verde.classList.add('apagado');
-            amarillo.classList.remove('apagado');
-            break;
-        case 'semaforo luz-verde-amarilla':
-            semaforo.className = 'semaforo luz-roja';
-            amarillo.classList.add('apagado');
-            rojo.classList.remove('apagado');
-            break;            
+btnCambiar.addEventListener("click", () => {
+    cambiarColorSemaforo();
+});
+
+function cambiarColorSemaforo(){
+    // Define el orden en el que se dirige el encendido (rojo-amarillo-verde o verde-amarillo-rojo)
+    if(luces[2].className.includes('encendido')){
+        luces.reverse();
     }
 
+    let indice = luces.findIndex(luz => luz.className.includes('encendido'))
+    luces.forEach(luz => luz.classList.remove('encendido'));
+
+    // Encender la luz siguiente a partir del indice anteriormente encendido
+    // Cambio de rojo a verde / cambio de verde-amarillo-rojo
+    luces[0].className.includes('rojo') ? luces[indice+2].classList.add('encendido') : luces[indice+1].classList.add('encendido');
 }
